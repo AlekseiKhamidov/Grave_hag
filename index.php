ccb7b7cd75e29acf4cc06356cbc50ad91b5aead1
AlekseiKhamidov <aleksei.khamidov@gmail.com>
2018-05-28 22:07:15 +0500

    Ignore config.php

--- index.php
+++ index.php
@@ -1,3 +1,16 @@
 <?php
-    
- ?>
+  require_once "vendor/autoload.php";
+  require_once "config.php";
+
+  try {
+    $amo = new \AmoCRM\Client(AMOCRM['subdomain'], AMOCRM['login'], AMOCRM['hash']);
+    echo "<pre>";
+    $account = $amo->account;
+    print_r($account->apiCurrent());
+    echo "</pre>";
+  } catch (\AmoCRM\Exception $e) {
+        printf('Error (%d): %s' . PHP_EOL, $e->getCode(), $e->getMessage());
+  }
+
+
+?>
