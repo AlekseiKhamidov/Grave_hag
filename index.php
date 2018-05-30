87ac8c815d2e497e846376fa542029bea6a1ae5f
AlekseiKhamidov <aleksei.khamidov@gmail.com>
2018-05-30 14:01:12 +0500

    Final stage of export phones and emails from amoCRM

--- index.php
+++ index.php
@@ -3,12 +3,8 @@
 <head>
   <meta charset="utf-8">
   <title>amoCRM Backup</title>
-  <meta charset="utf-8">
-  <meta name="description" content="Contacts & Companies">
-  <meta name="author" content="AlekseiKhamidov">
   <link rel="stylesheet" href="css/bootstrap.min.css">
   <link rel="stylesheet" href="css/bootstrap-table.min.css">
-
   <script src="js/jquery.min.js"></script>
   <script src="js/bootstrap.min.js"></script>
   <script src="js/bootstrap-table.js"></script>
@@ -22,20 +18,20 @@
         <tr>
            <th data-field="id">ID</th>
            <th data-field="Наименование">Наименование</th>
-           <th data-field="Город #1">Город</th>
-           <th data-field="Телефон #1">Телефон #1</th>
-           <th data-field="Телефон #2">Телефон #2</th>
-           <th data-field="Телефон #3">Телефон #3</th>
-           <th data-field="Телефон #4">Телефон #4</th>
-           <th data-field="Телефон #5">Телефон #5</th>
-           <th data-field="Телефон #6">Телефон #6</th>
-           <th data-field="Телефон #7">Телефон #7</th>
-           <th data-field="Телефон #8">Телефон #8</th>
-           <th data-field="Телефон #9">Телефон #9</th>
-           <th data-field="Email #1">Email #1</th>
-           <th data-field="Email #2">Email #2</th>
-           <th data-field="Email #3">Email #3</th>
-           <th data-field="Email #4">Email #4</th>
+           <th data-field="Город #1" data-formatter="clear">Город</th>
+           <th data-field="Телефон #1" data-formatter="clear">Телефон #1</th>
+           <th data-field="Телефон #2" data-formatter="clear">Телефон #2</th>
+           <th data-field="Телефон #3" data-formatter="clear">Телефон #3</th>
+           <th data-field="Телефон #4" data-formatter="clear">Телефон #4</th>
+           <th data-field="Телефон #5" data-formatter="clear">Телефон #5</th>
+           <th data-field="Телефон #6" data-formatter="clear">Телефон #6</th>
+           <th data-field="Телефон #7" data-formatter="clear">Телефон #7</th>
+           <th data-field="Телефон #8" data-formatter="clear">Телефон #8</th>
+           <th data-field="Телефон #9" data-formatter="clear">Телефон #9</th>
+           <th data-field="Email #1" data-formatter="clear">Email #1</th>
+           <th data-field="Email #2" data-formatter="clear">Email #2</th>
+           <th data-field="Email #3" data-formatter="clear">Email #3</th>
+           <th data-field="Email #4" data-formatter="clear">Email #4</th>
            <th data-field="Email #5">Email #5</th>
            <th data-field="Email #6">Email #6</th>
    </thead>
@@ -45,6 +41,10 @@
     $('#tab').bootstrapTable({
      url: "amocrm.php"
     });
+    function clear(value, row) {
+      if (!value) value = "";
+      return value;
+    };
   </script>
 </body>
 </html>
